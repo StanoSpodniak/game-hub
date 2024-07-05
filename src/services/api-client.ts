6c9b0e80d529c0868aa054d71ed8bf7f9a0736bb
StanoSpodniak <stano28@gmail.com>
2024-07-05 21:22:22 +0200

    fixed minor bug

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '7dd9c7c63c4d45b2adb2ec1117479e80'
+        key: 'key'
     }
 })
